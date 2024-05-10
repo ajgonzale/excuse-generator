70b61876d0101461b5c53cde7a76fbcc9b53748b
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-05-10 05:23:33 +0000

    add functions to app.js about calculating random numbers and overwriting inner text in excuse paragraph

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function randomNumber(max = 1) {
+    return Math.floor(Math.random() * max);
+  }
+  const excuse = document.getElementById("excuse");
+
+  excuse.innerText =
+    who[randomNumber(3)] +
+    " " +
+    action[randomNumber(3)] +
+    " " +
+    what[randomNumber(2)] +
+    " " +
+    when[randomNumber(4)];
 };
